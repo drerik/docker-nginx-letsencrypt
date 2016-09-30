52129cce18aceb33daa33d6c4dfb2eae41e31eb9
Erik Kaareng-Sunde <esu@enonic.com>
2016-09-30 08:13:53 +0200

    fixed bugs in launcher.sh and added symlink to certificates

--- launcher.sh
+++ launcher.sh
@@ -1,13 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "Checking certificates."
 if [[ ! -e /etc/letsencrypt/live/$HOSTNAME/privkey.pem ]]
 then
   DOMAIN_CMD=$(echo $LETS_ENCRYPT_DOMAINS | sed 's/,/ -d /')
-  echo certbot -n certonly --agree-tos --standalone -t -m "$LETS_ENCRYPT_EMAIL" -d $(hostname -f) -d $DOMAIN_CMD
+  certbot -n certonly --agree-tos --standalone -t -m "$LETS_ENCRYPT_EMAIL" -d $(hostname -f) -d $DOMAIN_CMD
+  ls -s /etc/letsencrypt/live/$HOSTNAME /etc/letsencrypt/certs
 else
   certbot renew
 fi
 
 echo "Launcing nginx."
-eval nginx -g 'daemon off';
+nginx -g "daemon off;"
