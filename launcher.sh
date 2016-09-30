3c3d02735db9500875efcc5815223997c187446a
Erik Kaareng-Sunde <esu@enonic.com>
2016-09-30 08:47:12 +0200

    fixed hostname bug in launcher.sh and smarter extra domain handling

--- launcher.sh
+++ launcher.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-echo "Checking certificates."
-if [[ ! -e /etc/letsencrypt/live/$HOSTNAME/privkey.pem ]]
+echo "Checking certificates ( if /etc/letsencrypt/live/$(hostname -f)/privkey.pem exist )."
+if [[ ! -e /etc/letsencrypt/live/$(hostname -f)/privkey.pem ]]
 then
-  DOMAIN_CMD=$(echo $LETS_ENCRYPT_DOMAINS | sed 's/,/ -d /')
-  certbot -n certonly --agree-tos --standalone -t -m "$LETS_ENCRYPT_EMAIL" -d $(hostname -f) -d $DOMAIN_CMD
-  ls -s /etc/letsencrypt/live/$HOSTNAME /etc/letsencrypt/certs
+  if [[ ! "x$LETS_ENCRYPT_DOMAINS" == "x" ]]; then
+    DOMAIN_CMD="-d $(echo $LETS_ENCRYPT_DOMAINS | sed 's/,/ -d /')"
+  fi
+
+  certbot -n certonly --agree-tos --standalone -t -m "$LETS_ENCRYPT_EMAIL" -d $(hostname -f) $DOMAIN_CMD
+  ls -s /etc/letsencrypt/live/$(hostname -f) /etc/letsencrypt/certs
 else
   certbot renew
 fi
